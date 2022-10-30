7abd735fd885d4b8e78d718320159306fa3fe708
brunoVA101 <brunovalcantara@hotmail.com>
2022-10-30 18:28:17 -0300

    💄 Fix button icons display

---
+++ front_end/Mega_sena_frontTests/Data/DataGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mega_sena_front.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mega_sena_front.Data.Tests
+{
+    [TestClass()]
+    public class DataGeneratorTests
+    {
+        [TestMethod()]
+        public void MegaSenasTest()
+        {
+            try
+            {
+                MegaSena[] megas = DataGenerator.MegaSenas(6);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+    }
+}
